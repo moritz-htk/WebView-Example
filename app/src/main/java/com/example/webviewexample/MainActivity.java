9cb7c9ee19a331efbbc9aa10d228f8dd66f0349d
Your Name <you@example.com>
2022-12-14 14:09:26 +0100

    WebView initialized

--- app/src/main/java/com/example/webviewexample/MainActivity.java
+++ app/src/main/java/com/example/webviewexample/MainActivity.java
@@ -1,14 +1,23 @@
 package com.example.webviewexample;
 
+import android.os.Bundle;
+import android.webkit.WebView;
+
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.os.Bundle;
-
 public class MainActivity extends AppCompatActivity {
+
+    WebView webView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        initWebView();
+    }
+
+    protected void initWebView() {
+        webView = findViewById(R.id.web_view);
+        webView.loadUrl("https://www.example.com/");
     }
 }
