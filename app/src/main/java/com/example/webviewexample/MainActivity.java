9d4552ea80cc8f3f51fe3f2fd4b6cd3d70437b6f
Your Name <you@example.com>
2022-12-14 14:21:52 +0100

    added WebView settings

--- app/src/main/java/com/example/webviewexample/MainActivity.java
+++ app/src/main/java/com/example/webviewexample/MainActivity.java
@@ -1,6 +1,7 @@
 package com.example.webviewexample;
 
 import android.os.Bundle;
+import android.webkit.WebSettings;
 import android.webkit.WebView;
 
 import androidx.appcompat.app.AppCompatActivity;
@@ -14,10 +15,18 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         initWebView();
+        initWebSettings();
     }
 
     protected void initWebView() {
         webView = findViewById(R.id.web_view);
         webView.loadUrl("https://www.example.com/");
+    }
+
+    protected void initWebSettings() {
+        WebSettings webSettings = webView.getSettings();
+        webSettings.setJavaScriptEnabled(true);
+        webSettings.setBuiltInZoomControls(true);
+        webSettings.setDisplayZoomControls(false);
     }
 }
